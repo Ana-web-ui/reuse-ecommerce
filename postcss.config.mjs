7cfa2e3f9b032e280bab8a196c4a04a2c964fbda
ana leite <anacarolinaleite1401@gmail.com>
2025-09-04 22:28:54 -0300

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,3 +3,13 @@ const config = {
 };
 
 export default config;
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
